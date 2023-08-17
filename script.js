87d5d0f9a1ab5063ad18ae3d65cf8b14cf9f8f55
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-17 23:06:11 +0200

    added cost calculation formula

---
+++ script.js
@@ -0,0 +1,15 @@
+function getFuelConsumption() {
+
+}
+
+function getDistance() {
+
+}
+
+function getFuelPrice() {
+
+}
+
+function calculateCost(kmpl, distance, fuelPrice) {
+    return (distance / kmpl) * fuelPrice;
+}
