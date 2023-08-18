37ab3ecdc766fce432bf3d94576da262af93375c
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-18 17:54:08 +0200

    getDistance method done

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function getFuelConsumption() {
-
+    return 14.0;
 }
 
 function getDistance() {
-
+    return document.getElementById('distance').value;
 }
 
 function getFuelPrice() {
-
+    return 2.62;
 }
 
 function calculateCost(kmpl, distance, fuelPrice) {
     return (distance / kmpl) * fuelPrice;
 }
+
